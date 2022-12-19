d1b58cf4edac58430502b9d2d9e86cdfd636880e
Mateusz Skiba <mateusz.skiba858@gmail.com>
2022-12-19 16:02:12 +0100

    cart checkout

---
+++ react-clothes/src/routes/checkout/checkout.components.jsx
@@ -0,0 +1,28 @@
+import "./checkout.styles.scss";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+const Checkout = () => {
+  const { cartItems, addItemToCart } = useContext(CartContext);
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+              <br />
+              <span>decrement</span>
+              <br />
+              <span onClick={addItemToCart}>increment</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
